3774119917d66a48dc517752676bf5d70ce6b4bc
dapmas <sampadmedda@gmail.com>
2017-04-08 22:36:09 +0530

    Created Posts Viwes and Controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,30 @@
 class PostsController < ApplicationController
 	def index
+	  @posts = "This is some sample text number 1 for the blog posts."
 	end
+	
+	def new
+
+	end
+
+	def create
+
+	end
+
+	def edit
+	
+	end
+
+	def update
+
+	end
+
+	def show
+
+	end
+
+	def destroy
+
+	end
+
 end
