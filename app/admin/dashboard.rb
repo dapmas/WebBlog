f1b8dfaab5724b3c9f431140d588d7ec18df9080
dapmas <sampadmedda@gmail.com>
2017-04-10 14:29:56 +0530

    Added Posts and Category hndlers to admin and updated the backend

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,33 +1,36 @@
 ActiveAdmin.register_page "Dashboard" do
-
-  menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
-
-  content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+#ActiveAdmin::Dashboards.build do
+  content :title => proc{ I18n.t("active_admin.dashboard") } do
+    section do
+      columns do
+        panel "Recent Posts" do
+          table_for Post.order("id desc").limit(5) do
+            column :id
+            column "Post Title", :title do |post|
+              link_to post.title, [:admin, post]
+            end
+            column :category, :sortable => :category
+            column :created_at
+          end
+          strong (link_to "Show All Posts" , :admin_posts )
+        end
       end
     end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    section do
+      columns do
+        panel "Categories" do
+          table_for Category.order("id desc").limit(5) do
+            column :id
+            column "Category Name", :name do |cat|
+              link_to cat.name, [:admin, cat]
+            end
+            column :created_at
+          end
+          strong (link_to "Show All Categories" , :admin_categories )
+        end
+      end
+    end
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+  end
 end
